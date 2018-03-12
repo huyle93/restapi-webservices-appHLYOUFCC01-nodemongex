711d72f3a24f5c913d6da8e5cacd88f45ac1458c
Huy Le <huyle.finance@gmail.com>
2018-03-12 00:28:05 -0400

    set up routes, tested first post request, received post request at host 8000 via postman. Set up mongo next

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+require('./app/routes')(app, {});
+app.listen(port, () => [
+    console.log("Magic is on " + port)
+])
