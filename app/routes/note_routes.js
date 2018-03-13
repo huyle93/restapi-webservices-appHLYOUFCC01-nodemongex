136c731c355f93ac94af90888966413bffcb7a5f
Huy Le <huyle.finance@gmail.com>
2018-03-13 00:54:00 -0400

    added put request. worked. tested. finished

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -40,15 +40,16 @@ module.exports = function(app, db) {
         });
     });
     // PUT
-    // app.get('/notes/:id', (req, res) => {
-    //     const id = req.params.id;
-    //     const details = {'_id': new ObjectID(id) };
-    //     db.collection('notes').findOne(details, (err, item) => {
-    //         if (err) {
-    //             res.send({'error': 'An error has occured'});
-    //         } else {
-    //             res.send(item);
-    //         }
-    //     });
-    // });
+    app.put('/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = {'_id': new ObjectID(id) };
+		const note = { text: req.body.body, title: req.body.title };
+		db.collection('notes').update(details, note, (err, item) => {
+			if (err) {
+				res.send({ 'error': 'An error has occured' });
+			} else {
+				res.send(item);
+			}
+		});
+	});
 };
