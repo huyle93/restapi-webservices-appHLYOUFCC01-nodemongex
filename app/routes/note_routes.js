f2006ceff9fb6413aa421f9fea37e2ef49fa623a
Huy Le <huyle.finance@gmail.com>
2018-03-13 00:34:07 -0400

    added get and delete request, worked, finished. working on put request to update current document. tested with postman.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,7 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+    // POST
     app.post('/notes', (req, res) => {
         const note = {text: req.body.body, title: req.body.title};
         db.collection('notes').insert(note, (err, result) => {
@@ -12,4 +15,40 @@ module.exports = function(app, db) {
         // console.log(req.body);
         // res.send('hello world');
     });
+    // GET
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+    // DELETE
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({'error': 'An error has occured'});
+            } else {
+                res.send('Note ' + id + ' is removed');
+            }
+        });
+    });
+    // PUT
+    // app.get('/notes/:id', (req, res) => {
+    //     const id = req.params.id;
+    //     const details = {'_id': new ObjectID(id) };
+    //     db.collection('notes').findOne(details, (err, item) => {
+    //         if (err) {
+    //             res.send({'error': 'An error has occured'});
+    //         } else {
+    //             res.send(item);
+    //         }
+    //     });
+    // });
 };
